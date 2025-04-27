242d46dd0a0f4a8f286733a6e199164ee9a4a52c
pranaythombare20 <thombarepranay65@gmail.com>
2025-04-27 21:26:44 +0530

    Add files via upload

--- db_connect.php
+++ db_connect.php
@@ -4,11 +4,9 @@ $username = "root";
 $password = "";
 $database = "student_tracking_db";
 
-// Using the procedural style
-$conn = mysqli_connect($servername, $username, $password, $database);
+$conn = new mysqli($servername, $username, $password, $database);
 
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
