93f1dcdbd212f17e535285d9ac620f79b7e1448e
pranaythombare20 <thombarepranay65@gmail.com>
2025-04-28 01:52:14 +0530

    Add files via upload

--- db_connect.php
+++ db_connect.php
@@ -1,20 +1,20 @@
 
 <?php
-$servername = "127.0.0.1"; // Use 127.0.0.1 instead of localhost
+$servername = "localhost";
 $username = "root";
 $password = "";
 $database = "student_tracking_db";
 
 try {
     // Create a new PDO instance
-    $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+    $conn = new PDO("mysql:host=127.0.0.1;dbname=$database", $username, $password);
+
 
     // Set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
-
 ?>
 <!-- error:
 Connection failed: could not find driver -->
